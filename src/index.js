4b6d88e200b40060066a7399058f5e5a23174cd8
LoboArkano <pipiolo_mananes@hotmail.com>
2021-01-08 09:22:52 -0600

    Create store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import App from './components/App';
+import rootReducer from './reducers/index';
+
+const INITIAL_STATE = {
+  monsters: {
+    loading: false,
+    error: '',
+    list: [],
+  },
+};
+const store = createStore(rootReducer, INITIAL_STATE, applyMiddleware(thunkMiddleware));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
