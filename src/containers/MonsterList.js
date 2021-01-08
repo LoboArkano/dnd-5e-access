b1684555b6c6374ae7bc6e7280b23f114e182540
LoboArkano <pipiolo_mananes@hotmail.com>
2021-01-08 09:23:43 -0600

    Get data from redux store

--- src/containers/MonsterList.js
+++ src/containers/MonsterList.js
@@ -1,33 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import apiDnD from '../api/apiDnD';
+import React, { useEffect } from 'react';
+import { connect, useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import fetchMonsters from '../actions/index';
 
-const MonsterList = () => {
-  const [state, setState] = useState({
-    loading: false,
-    monsters: {
-      results: [],
-    },
-  });
+const MonsterList = props => {
+  const { monsters } = props;
+  const { loading, list } = monsters;
+  const dispatch = useDispatch();
 
-  useEffect(async () => {
-    setState({
-      ...state,
-      loading: true,
-    });
-    const response = await apiDnD();
-    setState({
-      ...state,
-      loading: false,
-      monsters: response,
-    });
+  useEffect(() => {
+    dispatch(fetchMonsters('?limit=1500'));
   }, []);
 
   return (
     <div>
       {
-        state.loading
-          ? <h4>Loaging</h4>
-          : state.monsters.results.map(monster => {
+        loading
+          ? <h4>Loading</h4>
+          : list.map(monster => {
             const name = `Name: ${monster.name}`;
             const type = `Type: ${monster.type}`;
             const challengeRating = `Challenge Rating: ${monster.challenge_rating}`;
@@ -49,4 +39,19 @@ const MonsterList = () => {
   );
 };
 
-export default MonsterList;
+MonsterList.propTypes = {
+  monsters: PropTypes.shape({
+    loading: PropTypes.bool.isRequired,
+    list: PropTypes.arrayOf(PropTypes.shape().isRequired).isRequired,
+  }).isRequired,
+};
+
+const mapStateToProps = state => {
+  const { monsters } = state;
+
+  return { monsters };
+};
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MonsterList);
