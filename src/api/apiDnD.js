76762d92e9981dd5f35e2fb7536f67c36e3a8942
LoboArkano <pipiolo_mananes@hotmail.com>
2021-01-08 09:26:00 -0600

    Update to use custom url

--- src/api/apiDnD.js
+++ src/api/apiDnD.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const urlApi = 'https://api.open5e.com/monsters/?limit=1500';
+const urlApi = 'https://api.open5e.com/monsters/';
 
-const apiDnD = async () => {
-  const response = await axios.get(urlApi);
+const apiDnD = async (opt = '') => {
+  const response = await axios.get(`${urlApi}${opt}`);
 
   return response.data;
 };
