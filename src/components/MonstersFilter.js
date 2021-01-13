5c69ff039f149fb216eeab02cbe3b2bcb3ab8156
LoboArkano <pipiolo_mananes@hotmail.com>
2021-01-12 22:44:20 -0600

    Add style to MonstersFilter component

--- src/components/MonstersFilter.js
+++ src/components/MonstersFilter.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../assets/stylesheets/monstersFilter.css';
 
 const types = [
-  'All', 'humanoid', 'humanoid (any race)', 'swarm of tiny beasts', 'swarm of tiny constructs',
-  'aberration',
+  'All', 'Aberration', 'Beast', 'Celestial', 'Construct', 'Dragon', 'Elemental',
+  'Fey', 'Fiend', 'Giant', 'Humanoid', 'Humanoid (Any Race)', 'Humanoid (Shadow Fey)',
+  'Humanoid (Shapechanger', 'Humanoids', 'Monstrosity', 'Ooze', 'Plant',
+  'Swarm of Medium Undead', 'Swarm of Tiny Aberrations', 'Swarm of Tiny Beasts',
+  'Swarm of Tiny Constructs', 'Swarm of Tiny Elementals', 'Swarm of Tiny Fey',
+  'Swarm of Tiny Monstrosities', 'Swarm of Tiny Undead', 'Undead',
 ];
 
 const sizes = [
@@ -14,18 +19,18 @@ const MonstersFilter = props => {
   const { changeFilter } = props;
 
   return (
-    <div>
-      <label htmlFor="type">
+    <div className="filter-container d-flex w-100">
+      <label className="label d-flex" htmlFor="type">
         Type:
-        <select name="types" id="types" onChange={changeFilter}>
+        <select className="filter cap" name="types" id="types" onChange={changeFilter}>
           {types.map(type => (
             <option key={type} value={type}>{type}</option>
           ))}
         </select>
       </label>
-      <label htmlFor="size">
+      <label className="label d-flex" htmlFor="size">
         Size:
-        <select name="sizes" id="sizes" onChange={changeFilter}>
+        <select className="filter cap" name="sizes" id="sizes" onChange={changeFilter}>
           {sizes.map(size => (
             <option key={size} value={size}>{size}</option>
           ))}
