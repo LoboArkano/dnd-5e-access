00e7d909e46dfde8afec7b2f43bff2b86a297559
LoboArkano <pipiolo_mananes@hotmail.com>
2021-01-12 21:03:41 -0600

    Add style to footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import '../assets/stylesheets/footer.css';
 
 const Footer = () => (
-  <footer>
-    Hello from Footer
+  <footer className="footer-container w-100">
+    Data provided for
+    <a href="https://api.open5e.com/monsters/" className="link"> Open5e API</a>
   </footer>
 );
 
