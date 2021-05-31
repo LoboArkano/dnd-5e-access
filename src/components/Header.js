9405ea7a3cdfc40d1fa1c5de12c2a3991a000c62
LoboArkano <pipiolo_mananes@hotmail.com>
2021-05-31 13:18:24 -0500

    Update Header to css-in-jss

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../assets/stylesheets/header.css';
+import { createUseStyles } from 'react-jss';
 
-const Header = () => (
-  <header className="container">
-    <nav>
-      <ul>
-        <li>
-          <Link to="/" className="brand">D&D 5e Access</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+const useStyles = createUseStyles({
+  myheader: {
+    padding: [20, 25],
+    backgroundColor: 'rgb(214, 2, 2)',
+
+    '& a': {
+      color: 'rgb(247, 242, 242)',
+      textDecoration: 'none',
+      fontSize: 40,
+      fontFamily: ['MedievalSharp', 'Arial', 'Helvetica', 'sans-serif'],
+    },
+  },
+});
+
+const Header = () => {
+  const classes = useStyles();
+
+  return (
+    <header className={classes.myheader}>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">D&D 5e Access</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
