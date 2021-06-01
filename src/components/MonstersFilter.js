8f742d98cbb04bd6a56ece92d5d872c931d8f246
LoboArkano <pipiolo_mananes@hotmail.com>
2021-05-31 20:44:38 -0500

    Update MonstersFilter style

--- src/components/MonstersFilter.js
+++ src/components/MonstersFilter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import '../assets/stylesheets/monstersFilter.css';
+import { createUseStyles } from 'react-jss';
+import arrow from '../assets/images/down-arrow.png';
 
 const types = [
   'All', 'Aberration', 'Beast', 'Celestial', 'Construct', 'Dragon', 'Elemental',
@@ -15,22 +16,85 @@ const sizes = [
   'All', 'Tiny', 'Small', 'Medium', 'Large', 'Huge', 'Gargantuan',
 ];
 
+const useStyles = createUseStyles({
+  container: {
+    display: 'flex',
+    width: '100%',
+    margin: {
+      bottom: 40,
+      top: 30,
+    },
+    justifyContent: 'flex-end',
+  },
+
+  label: {
+    display: 'flex',
+    alignItems: 'center',
+    fontFamily: ['Merriweather', 'Arial', 'Helvetica', 'sans-serif'],
+    fontSize: 20,
+    color: 'rgb(22, 21, 21)',
+    fontWeight: 700,
+
+    '&:first-child': {
+      marginRight: 15,
+    },
+  },
+
+  filter: {
+    width: 250,
+    marginLeft: 10,
+    padding: [6, 10, 9, 10],
+    borderRadius: '4%',
+    fontSize: 16,
+    fontWeight: 400,
+    color: 'rgb(160, 156, 156)',
+    backgroundColor: 'rgb(247, 242, 242)',
+    border: [1, 'solid', 'rgb(160, 156, 156)'],
+    appearance: 'none',
+    backgroundImage: `url(${arrow})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 15,
+    backgroundPosition: '90%',
+    textTransform: 'capitalize',
+  },
+
+  '@media (max-width: 697px)': {
+    container: {
+      flexWrap: 'wrap',
+      marginBottom: 15,
+    },
+
+    label: {
+      width: '100%',
+
+      '&:first-child': {
+        marginRight: 0,
+      },
+    },
+
+    filter: {
+      flexGrow: 1,
+    },
+  },
+});
+
 const MonstersFilter = props => {
   const { changeFilter } = props;
+  const classes = useStyles();
 
   return (
-    <div className="filter-container d-flex w-100">
-      <label className="label d-flex" htmlFor="type">
+    <div className={classes.container}>
+      <label className={classes.label} htmlFor="type">
         Type:
-        <select className="filter cap" name="types" id="types" onChange={changeFilter}>
+        <select className={classes.filter} name="types" id="types" onChange={changeFilter}>
           {types.map(type => (
             <option key={type} value={type}>{type}</option>
           ))}
         </select>
       </label>
-      <label className="label d-flex" htmlFor="size">
+      <label className={classes.label} htmlFor="size">
         Size:
-        <select className="filter cap" name="sizes" id="sizes" onChange={changeFilter}>
+        <select className={classes.filter} name="sizes" id="sizes" onChange={changeFilter}>
           {sizes.map(size => (
             <option key={size} value={size}>{size}</option>
           ))}
